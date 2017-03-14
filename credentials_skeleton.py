12796a0e9b763558705729a3ee0e3479f656342e
Zach Lee <zach3lee@gmail.com>
2017-03-13 18:28:09 -0700

    created credentials and added gitignore

---
+++ credentials_skeleton.py
@@ -0,0 +1,19 @@
+# credentials_skeleton.py
+
+# create a copy of this and name it credentials.py
+# Remember, this is secret information! If you upload to GitHub, you need to make sure
+# 	you put the name of this file in the .gitignore file, otherwise your secret
+#	credentials will be on the internet for everyone to see!
+
+# Twilio
+# you can find these strings here: 
+#	https://www.twilio.com/console
+TWILIO_ACCOUNT_SID = ...
+TWILIO_AUTH_TOKEN = ...
+
+# flask
+FLASK_SECRET_KEY = ...
+# generated the secret key here
+# http://flask.pocoo.org/docs/0.12/quickstart/
+# ctrl-f "secret key"
+
